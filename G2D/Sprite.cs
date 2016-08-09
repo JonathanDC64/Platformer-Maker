e5a37e9799f467b7bc0904c655cf755aff8a4ba9
EpicNameBro <nintendo_jon64@hotmail.com>
2016-08-09 11:26:00 -0400

    Initial Commit

---
+++ G2D/Sprite.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer_Maker.G2D
+{
+    class Sprite
+    {
+        public Sprite(Texture2D texture, Rectangle rect, Vector2 origin, float rotation)
+        {
+            Texture = texture;
+            Rect = rect;
+            Center = origin;
+            Rotation = rotation;
+        }
+
+        public Sprite(Texture2D texture, Rectangle rect) : this(texture, rect, Vector2.Zero, 0.0f) {   }
+
+        public Sprite(Texture2D texture, Vector2 position, Vector2 bounds) : this(texture, new Rectangle((int)position.X, (int)position.Y, (int)bounds.X, (int)bounds.Y)){  }
+
+        public Sprite(Sprite sprite) : this(sprite.Texture, sprite.Rect, sprite.Center, sprite.Rotation) {  }
+
+        public Texture2D Texture { get; set; }
+
+        public int X
+        {
+            get
+            {
+                return rect.X;
+            }
+            set
+            {
+                rect.X = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return rect.Y;
+            }
+            set
+            {
+                rect.Y = value;
+            }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(X, Y);
+            }
+            set
+            {
+                X = (int)value.X;
+                Y = (int)value.Y;
+            }
+        }
+
+        public void SetPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void translate(int xAmount, int yAmount)
+        {
+            translateX(xAmount);
+            translateY(yAmount);
+        }
+
+        public void translateX(int xAmount)
+        {
+            X += xAmount;
+        }
+
+        public void translateY(int yAmount)
+        {
+            Y += yAmount;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return rect.Width;
+            }
+            set
+            {
+                rect.Width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return rect.Height;
+            }
+            set
+            {
+                rect.Height = value;
+            }
+        }
+
+        public Rectangle Rect
+        {
+            get
+            {
+                return rect;
+            }
+            set
+            {
+                rect = value;
+            }
+        }
+
+        private Rectangle rect;
+
+        public Vector2 Center
+        {
+            get
+            {
+                return center;
+            }
+            set
+            {
+                center = value;
+            }
+        }
+
+        private Vector2 center;
+
+        public void SetCenterX(float x)
+        {
+            center.X = x * Texture.Width;
+        }
+
+        public void SetCenterY(float y)
+        {
+            center.Y = y * Texture.Height;
+        }
+
+        public void SetCenter(float x, float y)
+        {
+            SetCenterX(x);
+            SetCenterY(y);
+        }
+
+        public float Rotation
+        {
+            get
+            {
+                return rotation;
+            }
+            set
+            {
+                rotation = value % (2.0f * MathHelper.Pi);
+            }
+        }
+
+        private float rotation;
+    }
+}
---
+++ G2D/TileSet.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer_Maker.G2D
+{
+    class Tileset
+    {
+        public Tileset(Texture2D tilesetTextre, int width, int height, int tileDimensions)
+        {
+            TilesetTexture = tilesetTextre;
+            Width = width;
+            Height = height;
+            TileDimensions = tileDimensions;
+        }
+
+        public Texture2D TilesetTexture { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public int TileDimensions { get; }
+
+        public Texture2D GetTile(int id)
+        {
+            int dimensions = TileDimensions * TileDimensions;
+            Color[] tileData = new Color[dimensions];
+
+            int x = 0;
+            int y = 0;
+            for(int i = 0; i < id; i++)
+            {
+                x++;
+                if(i != 0 && (i + 1) % Width == 0)
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+
+            Rectangle sourceRectanlge = new Rectangle(x * TileDimensions, y * TileDimensions, TileDimensions, TileDimensions);
+
+            TilesetTexture.GetData<Color>(0, sourceRectanlge, tileData, 0, dimensions);
+
+            Texture2D tile = new Texture2D(TilesetTexture.GraphicsDevice, TileDimensions, TileDimensions);
+            tile.SetData<Color>(tileData);
+
+            return tile;
+        }
+    }
+}
