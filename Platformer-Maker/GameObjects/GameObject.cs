3fd8720e3bf29e734232a42f69991bdbb34d71c8
EpicNameBro <nintendo_jon64@hotmail.com>
2016-08-24 16:03:05 -0400

    Made GameObject a subclass of Entity and created a State/Animation system

--- Platformer-Maker/GameObjects/GameObject.cs
+++ Platformer-Maker/GameObjects/GameObject.cs
@@ -2,15 +2,42 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Platformer_Maker.G2D;
+using System;
 using System.Collections.Generic;
 
 namespace Platformer_Maker.GameObjects
 {
-	public class GameObject
+	/// <summary>
+	/// A GameObject for levels
+	/// Can be a block, an enemie, etc..
+	/// </summary>
+	/// <typeparam name="StateEnum">The generic type T represents the Enum used for the objects state</typeparam>
+	public abstract class GameObject<StateEnum> : Entity
 	{
+		/// <summary>
+		/// Information on the game object
+		/// (Id, Name, etc).
+		/// Used in serialization.
+		/// </summary>
+		public GameObjectProperties Properties { get; set; }
 
-		public GameObjectProperties Properties;
+		/// <summary>
+		/// Dictionary of animations associated with
+		/// a game object state. Should be initialized
+		/// in the Initialize Function
+		/// </summary>
+		public Dictionary<StateEnum, AnimatedSprite> Sprites { get; set; }
 
+		public StateEnum State { get; }
+
+		public AnimatedSprite CurrentSprite
+		{
+			get
+			{
+				return Sprites[State];
+			}
+		}
+		
 		/// <summary>
 		/// Creates a Game Object
 		/// </summary>
@@ -19,15 +46,30 @@ namespace Platformer_Maker.GameObjects
 		///		Associative Array of animated sprites for when the 
 		///		Game Object has multiple states of animation (state, animation)
 		/// </param>
-		public GameObject(GameObjectProperties properties, Dictionary<string, AnimatedSprite> sprites)
+		public GameObject(GameObjectProperties properties)
 		{
 			Properties = properties;
 		}
 
 		/// <summary>
-		/// Insert behaviour for a game object in here
+		/// Insert all the necessary things to create the Game Object
+		/// Ex: Sprites
+		/// </summary>
+		public abstract void Initialize();
+
+		/// <summary>
+		/// Insert behaviour for a game object and
+		/// update object state here
 		/// </summary>
 		/// <param name="gameTime"></param>
-		public virtual void Update(GameTime gameTime) { }
+		public abstract void Update(GameTime gameTime);
+
+
+		protected void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+		{
+			CurrentSprite.Animate();
+			if(Properties.Visible)
+				spriteBatch.Draw(CurrentSprite.CurrentFrame, CurrentSprite.Rect, null, Color.White, CurrentSprite.Rotation, CurrentSprite.Center, SpriteEffects.None, 0);
+		}
 	}
 }
