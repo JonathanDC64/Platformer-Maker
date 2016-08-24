6669032f4f69c80280f28f37d9d07dd152b62adb
EpicNameBro <nintendo_jon64@hotmail.com>
2016-08-24 15:58:47 -0400

    Took all location and size based properties from sprite and put them in their own object: Entity

---
+++ Platformer-Maker/G2D/Entity.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Platformer_Maker.G2D
+{
+	public class Entity
+	{
+		public int X
+		{
+			get
+			{
+				return rect.X;
+			}
+			set
+			{
+				rect.X = value;
+			}
+		}
+
+		public int Y
+		{
+			get
+			{
+				return rect.Y;
+			}
+			set
+			{
+				rect.Y = value;
+			}
+		}
+
+		public Vector2 Position
+		{
+			get
+			{
+				return new Vector2(X, Y);
+			}
+			set
+			{
+				X = (int)value.X;
+				Y = (int)value.Y;
+			}
+		}
+
+		public void SetPosition(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public void translate(int xAmount, int yAmount)
+		{
+			translateX(xAmount);
+			translateY(yAmount);
+		}
+
+		public void translateX(int xAmount)
+		{
+			X += xAmount;
+		}
+
+		public void translateY(int yAmount)
+		{
+			Y += yAmount;
+		}
+
+		public int Width
+		{
+			get
+			{
+				return rect.Width;
+			}
+			set
+			{
+				rect.Width = value;
+			}
+		}
+
+		public int Height
+		{
+			get
+			{
+				return rect.Height;
+			}
+			set
+			{
+				rect.Height = value;
+			}
+		}
+
+		public Rectangle Rect
+		{
+			get
+			{
+				return rect;
+			}
+			set
+			{
+				rect = value;
+			}
+		}
+
+		private Rectangle rect;
+
+		public Vector2 Center
+		{
+			get
+			{
+				return center;
+			}
+			set
+			{
+				center = value;
+			}
+		}
+
+		private Vector2 center;
+
+		public void SetCenterX(float x)
+		{
+			//center.X = x * Textures[0].Width;
+			center.X = x * Width;
+		}
+
+		public void SetCenterY(float y)
+		{
+			//center.Y = y * Textures[0].Height;
+			center.Y = y * Height;
+		}
+
+		public void SetCenter(float x, float y)
+		{
+			SetCenterX(x);
+			SetCenterY(y);
+		}
+
+		public float Rotation
+		{
+			get
+			{
+				return rotation;
+			}
+			set
+			{
+				rotation = value % (2.0f * MathHelper.Pi);
+			}
+		}
+
+		private float rotation;
+	}
+}
--- Platformer-Maker/G2D/Sprite.cs
+++ Platformer-Maker/G2D/Sprite.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Platformer_Maker.G2D
 {
-    public class Sprite
+    public class Sprite : Entity
     {
         public Sprite(Texture2D texture, Rectangle rect, Vector2 origin, float rotation)
         {
@@ -11,7 +11,6 @@ namespace Platformer_Maker.G2D
             Rect = rect;
             Center = origin;
             Rotation = rotation;
-
         }
 
 		public Sprite() : this(null, Rectangle.Empty, Vector2.Zero, 0) { }
@@ -34,146 +33,5 @@ namespace Platformer_Maker.G2D
 		}
 
         protected Texture2D[] Textures { get; set; }
-
-        public int X
-        {
-            get
-            {
-                return rect.X;
-            }
-            set
-            {
-                rect.X = value;
-            }
-        }
-
-        public int Y
-        {
-            get
-            {
-                return rect.Y;
-            }
-            set
-            {
-                rect.Y = value;
-            }
-        }
-
-        public Vector2 Position
-        {
-            get
-            {
-                return new Vector2(X, Y);
-            }
-            set
-            {
-                X = (int)value.X;
-                Y = (int)value.Y;
-            }
-        }
-
-        public void SetPosition(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public void translate(int xAmount, int yAmount)
-        {
-            translateX(xAmount);
-            translateY(yAmount);
-        }
-
-        public void translateX(int xAmount)
-        {
-            X += xAmount;
-        }
-
-        public void translateY(int yAmount)
-        {
-            Y += yAmount;
-        }
-
-        public int Width
-        {
-            get
-            {
-                return rect.Width;
-            }
-            set
-            {
-                rect.Width = value;
-            }
-        }
-
-        public int Height
-        {
-            get
-            {
-                return rect.Height;
-            }
-            set
-            {
-                rect.Height = value;
-            }
-        }
-
-        public Rectangle Rect
-        {
-            get
-            {
-                return rect;
-            }
-            set
-            {
-                rect = value;
-            }
-        }
-
-        private Rectangle rect;
-
-        public Vector2 Center
-        {
-            get
-            {
-                return center;
-            }
-            set
-            {
-                center = value;
-            }
-        }
-
-        private Vector2 center;
-
-        public void SetCenterX(float x)
-        {
-            center.X = x * Textures[0].Width;
-        }
-
-        public void SetCenterY(float y)
-        {
-            center.Y = y * Textures[0].Height;
-        }
-
-        public void SetCenter(float x, float y)
-        {
-            SetCenterX(x);
-            SetCenterY(y);
-        }
-
-        public float Rotation
-        {
-            get
-            {
-                return rotation;
-            }
-            set
-            {
-                rotation = value % (2.0f * MathHelper.Pi);
-            }
-        }
-
-        private float rotation;
     }
 }
